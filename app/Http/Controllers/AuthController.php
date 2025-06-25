458b8c9834f64d8ac67c00ad72e00eb1cb1eb1c0
Leandro Gonçalves <lleandrogon2004@gmail.com>
2025-06-25 10:55:56 -0300

    Ajustando método login no AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,7 +22,8 @@ class AuthController extends Controller
 
             return response()->json([
                 "access_token" => $token,
-                "token_type" => "Bearer"
+                "token_type" => "Bearer",
+                "user" => $user
             ], 200);
         }
 
